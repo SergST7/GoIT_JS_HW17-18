97029ed3386a11a714d0daa7949dcba9004760c2
Sergey Stelmakh <stelmakhserg@gmail.com>
2016-09-26 23:58:21 +0300

    adde gulp file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by SergST on 26.09.2016.
+ */
+var gulp = require("gulp");
+
+gulp.task('mytask', function() {
+	console.log('Привет, я таск!');
+});
+
+gulp.task('default', function() {
+	// place code for your default task here
+});
